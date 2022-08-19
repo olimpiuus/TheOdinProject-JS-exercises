51c091ba52b9d206333d0098f24652f2b81b0e43
olimpiuus <kolegin.yuriu@gmail.com>
2022-08-18 19:41:55 -0600

    find solution to make reverse str

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = ''
+    let arrayStr = [...str]
+        // arrayStr.forEach((word) => { newStr.push(word) })
+    for (let i = arrayStr.length - 1; i >= 0; i--) {
+        newStr += arrayStr[i]
+    }
+    return newStr
 };
-
 // Do not edit below this line
 module.exports = reverseString;
