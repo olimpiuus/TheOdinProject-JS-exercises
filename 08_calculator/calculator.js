47656821009c060840c966dfc7e6ad2b23a61aae
olimpiuus <kolegin.yuriu@gmail.com>
2022-08-22 21:29:36 -0600

    find solution for task

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    return arr.reduce((summ, num) => summ += num, 0)
+        // argmts.reduce(((summ, a) => summ += a), 0)
+
+
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+    return arr.reduce((total, a) => total *= a, 1)
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    let faq = 1
+    for (let i = a; i > 1; i--) {
+        faq = faq * i
+
+    }
+    return faq
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
