4946a0612da81d1bab2341ce0912ad2f23992eb1
olimpiuus <kolegin.yuriu@gmail.com>
2022-08-23 20:07:24 -0600

    find solution for fibonachi numbers

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if (a < 0) { return 'OOPS' }
+    let fibo = [1, 1]
+    for (let i = 0; i < a; i++) {
+        fibo[i + 2] = fibo[i + 1] + fibo[i]
+    }
 
+    console.log(fibo);
+    return fibo[a - 1]
 };
 
 // Do not edit below this line
