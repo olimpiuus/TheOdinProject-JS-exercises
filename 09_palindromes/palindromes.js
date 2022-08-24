3353b1da7924833be3ab7c64135c2e5847bb19af
olimpiuus <kolegin.yuriu@gmail.com>
2022-08-23 19:55:04 -0600

    find solution for polidromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function(str) {
+    let newStr = ''
+    let copyStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
 
+    let newArrStr = [...copyStr]
+    for (let i = newArrStr.length - 1; i >= 0; i--) {
+        newStr += newArrStr[i]
+    }
+    console.log(newStr, str.toLowerCase());
+    return newStr === copyStr
 };
 
 // Do not edit below this line
